430ed0161494689789d469befa27023a2b4aa620
Aravind Surya Teja Motapothula <motapothula.aravindsuryateja@gmail.com>
2026-06-28 12:39:53 +0000

    committed after using macro

--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -1,24 +1,11 @@
-with cte as (
-
-    select to_timestamp(STARTED_AT) as started_at,
-    date(to_timestamp(STARTED_AT)) as date_started_at,
-    hour(to_timestamp(STARTED_AT)) as hour_started_at,
-    case when dayname(to_timestamp(STARTED_AT)) in ('Sat','Sun')
-    then 'Weekend'
-    else 'Business Days'
-    end as day_type,
-    case when month(to_timestamp(STARTED_AT)) in (12,1,2)
-    then 'Winter'
-    when month(to_timestamp(STARTED_AT)) in (3,4,5)
-    then 'Spring'
-    when month(to_timestamp(STARTED_AT)) in (6,7,8)
-    then 'Summer'
-    else 'Autumn'
-    end station_of_the_year
-    from 
-    {{ source('demo', 'bike') }}
-    where STARTED_AT !='started_at'
-)
-
-
-select * from cte
+WITH cte AS (
+    SELECT 
+        TO_TIMESTAMP(started_at) AS started_at, 
+        DATE(TO_TIMESTAMP(started_at)) AS date_started_at, 
+        HOUR(TO_TIMESTAMP(started_at)) AS hour_started_at, 
+        {{ get_date('STARTED_AT') }} AS day_type, 
+        {{ get_season('STARTED_AT') }} AS station_of_the_year 
+    FROM {{ source('demo', 'bike') }} 
+    WHERE started_at != 'started_at' 
+) 
+SELECT * FROM cte
